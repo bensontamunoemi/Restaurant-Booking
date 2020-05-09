07f997e1357bc3eca2ae48011c9dbe8b37126003
massive5566 <62636484+massive5566@users.noreply.github.com>
2020-05-10 01:38:26 +0400

    Update credentials.php

--- admin/credentials.php
+++ admin/credentials.php
@@ -1,9 +1,14 @@
 <?php
 //Database credentials
 
-$host = "z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user = "lm02ee6xeoqshsjt";
-$password = "yuvuvvpz1oso1s8r";
-$database = "s5y9n2shnjy5h09f";
-?>
+if (process.env.JAWSDB_URL) {
+  connection = mysql_createConnection(process.env.JAWSDB_URL)
+}else {
+  $host = "localhost";
+  $user = "root";
+  $password = "";
+  $database = "restaurant";
+}
 
+
+?>
