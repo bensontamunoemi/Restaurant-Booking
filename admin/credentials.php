a759b5bd10c325befed0ecc7734e86efdeb54362
massive5566 <62636484+massive5566@users.noreply.github.com>
2020-05-10 00:52:30 +0400

    Update credentials.php

--- admin/credentials.php
+++ admin/credentials.php
@@ -1,9 +1,9 @@
 <?php
 //Database credentials
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "restaurant";
+$host = "z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user = "lm02ee6xeoqshsjt";
+$password = "yuvuvvpz1oso1s8r";
+$database = "s5y9n2shnjy5h09f";
 ?>
 
